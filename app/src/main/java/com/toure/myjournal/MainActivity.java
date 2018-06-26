a8af033b63c80598cb83b09125ed08ecb427194b
Toure Nathan <toure21@live.com>
2018-06-26 12:05:43 +0100

    Added login and signup with firebase UI

--- app/src/main/java/com/toure/myjournal/MainActivity.java
+++ app/src/main/java/com/toure/myjournal/MainActivity.java
@@ -1,13 +1,125 @@
 package com.toure.myjournal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.toure.myjournal.util.Utility;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAC = MainActivity.class.getSimpleName();
+
+    // Firebase instance variable
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateLister;
+
+    private static final int RC_SIGN_IN = 1459;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        mAuthStateLister = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    //user is signed in
+                    Toast.makeText(MainActivity.this, "You are now login", Toast.LENGTH_LONG).show();
+
+                }
+                else {
+                    // user is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setTheme(R.style.GreenTheme)
+                                    .setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateLister);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateLister);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    finish();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_sign_out){
+            if (Utility.isOnline(this)){
+                AuthUI.getInstance()
+                        .signOut(this);
+            }
+            else {
+                showSnackbar(R.string.no_internet_connection);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void showSnackbar(int stringId){
+        Snackbar.make(findViewById(android.R.id.content), getString(stringId), Snackbar.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/toure/myjournal/util/Utility.java
@@ -0,0 +1,31 @@
+package com.toure.myjournal.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.net.InetAddress;
+
+/**
+ * Created by Toure Nathan on 6/26/2018.
+ */
+public class Utility {
+
+    public static boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("http://www.google.com");
+            //You can replace it with your name
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+}
