f63392f56c7a0937f9d40a1da0f667f5793bd031
Toure Nathan <toure21@live.com>
2018-06-29 16:44:01 +0100

    Completed the view for the detailactivity

--- app/src/main/java/com/toure/myjournal/DetailActivity.java
+++ app/src/main/java/com/toure/myjournal/DetailActivity.java
@@ -1,13 +1,10 @@
 package com.toure.myjournal;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -18,14 +15,14 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
